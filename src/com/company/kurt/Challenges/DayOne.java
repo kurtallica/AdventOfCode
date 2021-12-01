b9ab1e22b21fc91f4da53e9eef40245626fd506a
kurt <kurtallica@hotmail.com>
2021-12-01 21:00:04 +0000

    D1 T1 - Completed task

--- src/com/company/kurt/Challenges/DayOne.java
+++ src/com/company/kurt/Challenges/DayOne.java
@@ -2,6 +2,8 @@ package com.company.kurt.Challenges;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DayOne {
@@ -10,16 +12,17 @@ public class DayOne {
 
         File file = new File("DayOneTaskOne");
         Scanner sc = new Scanner(file);
+
+        int data = sc.nextInt();
         int count = 0;
-        int data = 0;
 
         try {
             while (sc.hasNext()) {
                 int scNext = sc.nextInt();
                 if(scNext > data){
                     count++;
-                    data = scNext;
                 }
+                data = scNext;
             }
             sc.close();
         } catch (Exception e) {
